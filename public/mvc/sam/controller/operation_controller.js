ea33ade2fb6d083d3ee61bb4e68b9bedc7c50fd9
gengtieming <yopming@server.local>
2014-07-01 16:39:48 +0800

    Filter function of topics;

--- public/mvc/sam/controller/operation_controller.js
+++ public/mvc/sam/controller/operation_controller.js
@@ -4,64 +4,79 @@ var samOperationControllers = angular.module('samOperationControllers', []);
  * Operation mail
  */
 samOperationControllers.controller('SamOperationMailCtrl', ['$scope', '$http',
-	function($scope, $http) {
-		$http.get('/api/personnel/all').success(function(data) {
-			$scope.personnels = data;
-		});
+  function($scope, $http) {
+    $http.get('/api/personnel/all').success(function(data) {
+      $scope.personnels = data;
+    });
 
-		$http.get('/api/task/mail/all').success(function(data) {
-			$scope.mails = data;
-		});
+    $http.get('/api/task/mail/all').success(function(data) {
+      $scope.mails = data;
+    });
 
-		$scope.selected_atom = [];
+    $scope.selected_atom = [];
 
-		$scope.filterMailByAtom = function(id) {
-			if (_.contains($scope.selected_atom, id)) {
-				$scope.selected_atom = _.without($scope.selected_atom, id);
-			} else {
-				$scope.selected_atom.push(id);
-			}
-		};
+    $scope.filterMailByAtom = function(id) {
+      if (_.contains($scope.selected_atom, id)) {
+        $scope.selected_atom = _.without($scope.selected_atom, id);
+      } else {
+        $scope.selected_atom.push(id);
+      }
+    };
 
-		$scope.isChecked = function(id) {
-			if (_.contains($scope.selected_atom, id)) {
-				return "checkmark icon";
-			}
-			return false;
-		};
-	}
+    $scope.isChecked = function(id) {
+      if (_.contains($scope.selected_atom, id)) {
+        return "checkmark icon";
+      }
+      return false;
+    };
+  }
 ]);
 
 
 samOperationControllers.controller('SamOperationTopicCtrl', ['$scope', '$http',
-	function($scope, $http) {
-		$http.get('/api/personnel/all').success(function(data) {
-			$scope.personnels = data;
-		});
+  function($scope, $http) {
+    $http.get('/api/personnel/all').success(function(data) {
+      $scope.personnels = data;
+    });
 
-		$http.get('/api/pipe/all').success(function(data) {
-			$scope.pipes = data;
-		});
+    $http.get('/api/pipe/all').success(function(data) {
+      $scope.pipes = data;
+    });
 
-		$http.get('/api/task/topic/all').success(function(data) {
-			$scope.topics = data;
-		});
+    $http.get('/api/task/topic/all').success(function(data) {
+      $scope.topics = data;
+    });
 
-		$scope.selected_atom = [];
+    $scope.selected_atom = {
+      "pipe": {"id": '', "name": ''},
+      "ga": {"id": '', "name": ''},
+      "fe": {"id": '', "name": ''}
+    };
 
-		$scope.filterTopicByAtom = function(id) {
-			if (_.contains($scope.selected_atom, id)) {
-				$scope.selected_atom = _.without($scope.selected_atom, id);
-			} else {
-				$scope.selected_atom.push(id);
-			}
-		};
+    $scope.filterTopicsByAtom = function(id, genre, name) {
+      if ($scope.selected_atom[genre].id == id) {
+        $scope.selected_atom[genre].id = '';
+        $scope.selected_atom[genre].name = '';
+      } else {
+        $scope.selected_atom[genre].id = id;
+        $scope.selected_atom[genre].name = name;
+      }
+      return false;
+    };
 
-		$scope.isChecked = function(id) {
-			if (_.contains($scope.selected_atom, id)) {
-				return "checkmark icon";
-			}
-			return false;
-		};
-	}
+    $scope.isChecked = function(id, genre) {
+      if ($scope.selected_atom[genre].id == id) {
+        return "checkmark icon";
+      }
+      return false;
+    };
+
+    $scope.removeCriteria = function(id) {
+      for (var i in $scope.selected_atom) {
+        if ($scope.selected_atom[i].id == id) {
+          $scope.selected_atom[i].id = '';
+        }
+      }
+    }
+  }
 ]);
--- public/mvc/sam/filter/operation_filter.js
+++ public/mvc/sam/filter/operation_filter.js
@@ -33,27 +33,57 @@ samOperationFilters.filter('SamOperationMailFilter', [
  * Topic
  */
 samOperationFilters.filter('SamOperationTopicFilter', [
-    function() {
-        return function(topics, selected_atom) {
-            if (!angular.isUndefined(topics) && !angular.isUndefined(selected_atom)) {
-                var _topics = topics;
-                angular.forEach(selected_atom, function(atom) {
-                    tempTopics = [];
-                    angular.forEach(_topics, function(topic) {
-                        if (
-                            angular.equals(topic.personnel_ga._id, atom) ||
-                            angular.equals(topic.personnel_fe._id, atom) ||
-                            angular.equals(topic.belong_to._id, atom)
-                            ) {
-                            tempTopics.push(topic);
-                        }
-                    });
-                    _topics = tempTopics;
-                });
-                return _topics;
-            } else {
-                return false;
-            }
+  function() {
+    // package the original equal evaluation
+    packagedAngularEquals = function(a, b) {
+      // judgement of whether b is blank should be regarded as the urgentest!
+      if (b == '') {
+        return true;
+      } else if (angular.isUndefined(a) || a === null) {
+        return false;
+      } else {
+        return angular.equals(a, b);
+      }
+    };
+
+    // package the original get value, incase the element is null or undefined
+    packagedGetAttr = function(el, node) {
+      if (el === null) {
+        return null;
+      } else {
+        return el[node];
+      }
+    };
+
+    return function(topics, selected_atom) {
+
+      if (!angular.isUndefined(topics) && !angular.isUndefined(selected_atom)) {
+        var _topics = topics;
+        tempTopics = [];
+        
+        // each creteria doesn't exists
+        if (
+          selected_atom.pipe.id == '' &&
+          selected_atom.ga.id == '' &&
+          selected_atom.fe.id == ''
+        ) {
+          return _topics;
         }
+
+        // meaningful creteria exists
+        angular.forEach(_topics, function(topic) {
+          if (
+            packagedAngularEquals(packagedGetAttr(topic.belong_to, '_id'), selected_atom.pipe.id) &&
+            packagedAngularEquals(packagedGetAttr(topic.personnel_ga, '_id'), selected_atom.ga.id) &&
+            packagedAngularEquals(packagedGetAttr(topic.personnel_fe, '_id'), selected_atom.fe.id) 
+            ) {
+            tempTopics.push(topic);
+          } 
+        });
+        return tempTopics;
+      } else {
+        return false;
+      }
     }
+  }
 ]);
