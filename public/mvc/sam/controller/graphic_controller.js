ce749e2e59891dd45654444f01fe540d3209595d
yopming <yopming@gmail.com>
2015-04-20 15:48:04 +0800

    update the stylesheet;

--- public/mvc/sam/controller/graphic_controller.js
+++ public/mvc/sam/controller/graphic_controller.js
@@ -6,7 +6,7 @@ var samGraphicControllers = angular.module('samGraphicControllers', []);
 samGraphicControllers.controller('SamGraphicCtrl', ['$scope', '$http',
     function($scope, $http) {
         $http.get('http://192.168.17.135:54321/files').success(function(data) {
-            $scope.nodes = data;
+            $scope.nodes = _.toArray(data);
         });
     }
 ]);
