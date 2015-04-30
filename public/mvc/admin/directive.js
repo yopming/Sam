3d1a876c221bf354d397a40b44f8c4f3b84f169b
yopming <yopming@gmail.com>
2015-04-30 11:17:51 +0800

    directive bug fix;

--- public/mvc/admin/directive.js
+++ public/mvc/admin/directive.js
@@ -4,22 +4,22 @@ var adminDirectives = angular.module('adminDirectives', []);
  * Detect the tab active class
  */
 adminDirectives.directive('adminActive', ['$location',
-  function($location) {
-    return {
-      restrict: 'A',
-      link: function postLink(scope, element, attrs, controller) {
-        var classname = attrs.adminActive;
-        var path = attrs.href;
-        path = path.substring(2); // '#!'
-        scope.location = $location;
-        scope.$watch('location.path()', function(newPath) {
-          if (path === newPath) {
-            element.addClass(classname);
-          } else {
-            element.removeClass(classname);
-          }
-        });
-      }
+    function($location) {
+        return {
+            restrict: 'A',
+            link: function postLink(scope, element, attrs, controller) {
+                var classname = attrs.adminActive;
+                var path = attrs.href;
+                path = path.substring(2); // '#!'
+                scope.location = $location;
+                scope.$watch('location.path()', function(newPath) {
+                    if (newPath.indexOf(path) === 0) {
+                        element.addClass(classname);
+                    } else {
+                        element.removeClass(classname);
+                    }
+                });
+            }
+        }
     }
-  }
 ]);
