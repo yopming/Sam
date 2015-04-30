7f2cd65fa04c9b03b14b23070f4909fa351c4156
yopming <yopming@gmail.com>
2015-04-30 10:55:00 +0800

    bugfix for username in admin;

--- routes/admin/route.js
+++ routes/admin/route.js
@@ -10,54 +10,60 @@ var query_mod = require('../../model/mod.js').queryNice;
 
 // sign
 exports.sign = function(req, res) {
-  if (req.session.user) {
-    res.redirect('/admin');
-  } else {
-    res.render('admin/sign');
-  }
+    if (req.session.user) {
+        res.redirect('/admin');
+    } else {
+        res.render('admin/sign');
+    }
 };
 
 exports.signin = function(req, res, next) {
-  authenticate(req.body.username, req.body.password, function(err, user) {
-    if (user) {
-      req.session.regenerate(function() {
-        req.session.user = user;
-        query_mod(user.group, function(group) {
-          req.session.group = group;
-          res.cookie('group', group.nice, {maxAge: 24*60*60*1000});
-          res.redirect('/admin');
-        });
-      });
-    } else {
-      req.flash('info', 'Username or password is not corret.');
-      res.redirect('/sign');
-    }
-  });
+    authenticate(req.body.username, req.body.password, function(err, user) {
+        if (user) {
+            req.session.regenerate(function() {
+                req.session.user = user;
+                query_mod(user.group, function(group) {
+                    req.session.group = group;
+                    res.cookie('group', group.nice, {
+                        maxAge: 24 * 60 * 60 * 1000
+                    });
+                    res.redirect('/admin');
+                });
+            });
+        } else {
+            req.flash('info', 'Username or password is not corret.');
+            res.redirect('/sign');
+        }
+    });
 };
 
 exports.signout = function(req, res) {
-  req.session.destroy();
-  res.clearCookie('group');
-  res.redirect('/');
+    req.session.destroy();
+    res.clearCookie('group');
+    res.redirect('/');
 };
 
 
 // admin
 exports.admin = function(req, res) {
-  query_mod(req.session.user.group, function(group) {
-    res.cookie('group', group.nice, {maxAge: 24*60*60*1000});
-    res.render('admin/admin', {current_user: req.session.user});
-  });
+    query_mod(req.session.user.group, function(group) {
+        res.cookie('group', group.nice, {
+            maxAge: 24 * 60 * 60 * 1000
+        });
+        res.render('admin/admin', {
+            current_user: req.session.user
+        });
+    });
 };
 
 
 // sift
 exports.admin_sift = function(req, res) {
-  res.render('admin/sift');
+    res.render('admin/sift');
 };
 
 
 // modify password
 exports.admin_password_edit = function(req, res) {
-  res.render('admin/password_edit');
+    res.render('admin/password_edit');
 };
